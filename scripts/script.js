14e53de29b1c4e917dab83ac8b66464528b7f8c2
Gian Carlo Gallegos González <ggallegosg1900@alumno.ipn.mx>
2021-11-20 22:08:54 -0600

    NoNeim v52

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(function(){
     $(window).scroll(function(){
         var winTop = $(window).scrollTop();
-        if(winTop >= 50){
+        if(winTop >= 30){
             $("body").addClass("sticky-header");
         }else{
             $("body").removeClass("sticky-header");
